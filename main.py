c7276d46f46b500add2677a14fcb197645e040cb
21396-lestreek <21396@hetstreek.nl>
2025-12-04 13:13:22 +0100

    pxt.json, main.ts, main.py bijwerken

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+lightvalue = 0
+kitronik_VIEW128x64.control_display_on_off(True)
+radio.set_group(52)
+def on_forever():
+    global lightvalue
+    lightvalue = pins.analog_read_pin(AnalogPin.P1)
+    kitronik_VIEW128x64.show(lightvalue)
+    kitronik_VIEW128x64.refresh()
+basic.forever(on_forever)
 def on_button_pressed_a():
-    print("aeaea")
+    radio.send_string("left")
 input.on_button_pressed(Button.A, on_button_pressed_a)
+def on_button_pressed_b():
+    radio.send_string("right")
+input.on_button_pressed(Button.B, on_button_pressed_b)
+def on_button_pressed_ab():
+    radio.send_string("both")
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+let lightvalue = 0
+kitronik_VIEW128x64.controlDisplayOnOff(true)
+radio.setGroup(52)
+basic.forever(function on_forever() {
+    
+    lightvalue = pins.analogReadPin(AnalogPin.P1)
+    kitronik_VIEW128x64.show(lightvalue)
+    kitronik_VIEW128x64.refresh()
+})
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    console.log("aeaea")
+    radio.sendString("left")
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    radio.sendString("right")
+})
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    radio.sendString("both")
 })
